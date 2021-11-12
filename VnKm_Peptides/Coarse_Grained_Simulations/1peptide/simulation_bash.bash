8ff5342dc148fc97cb0b1350e016e92158965053
akashban <52219868+akashban@users.noreply.github.com>
2021-11-11 23:27:32 -0500

    Update simulation_bash.bash

--- VnKm_Peptides/Coarse_Grained_Simulations/1peptide/simulation_bash.bash
+++ VnKm_Peptides/Coarse_Grained_Simulations/1peptide/simulation_bash.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#We have run this simulation on the following GROMACS versions : GROMACS 2016.6 and 2018.3
+#We have run this simulation on the following GROMACS versions : GROMACS 5, 2016.6, 2018.3, 2018 and 2020.2
 #The simulation has been tested on the following machines: 
 #1. Bridges2 (8 cores on the RM-shared queue)
 # link to Bridges2 userguide: https://www.psc.edu/resources/bridges-2/user-guide-2/
@@ -35,13 +35,13 @@ $exp_grompp grompp -f "${minim}.mdp" -c $GRO -o $tpr -p  $TOP -n $NDX
 
 # Please note that different version of GROMACS interpret tabulated potentials in various formats.
 
-## For Gromacs 2016.6 and 2018.3, use the following: 
+## For newer versions of Gromacs (Gromacs 2016.1 and beyond), use the following: 
 
 
 $exp_grompp mdrun  -deffnm $tpr -v -tableb table_b1222.xvg table_a101771.xvg  table_a208771.xvg  table_a44771.xvg  table_b2999.xvg table_a10771.xvg   table_a2771.xvg    table_a45771.xvg     table_b3999.xvg table_a141772.xvg  table_a31771.xvg   table_a5771.xvg      table_b4999.xvg table_a142772.xvg  table_a32771.xvg   table_a6771.xvg      table_b5999.xvg table_a151772.xvg  table_a33771.xvg   table_a8771.xvg      table_b6999.xvg table_a152772.xvg  table_a34771.xvg   table_a9771.xvg      table_b7999.xvg table_a1771.xvg    table_a35771.xvg   table_b10999.xvg  table_b8999.xvg table_a201771.xvg  table_a41771.xvg   table_b11999.xvg  table_b9999.xvg table_a202771.xvg  table_a42771.xvg   table_b12999.xvg   table_b8222.xvg table_a205772.xvg  table_a43771.xvg   table_b1999.xvg table_d1.xvg table_d2.xvg table_d3.xvg table_d4.xvg table_d5.xvg table_d6.xvg table_d7.xvg table_d8.xvg table_d9.xvg   table_a400777.xvg table_a401777.xvg table_a502777.xvg
 
 
-## For Gromacs 5.1.2, you may need to use it in the following mannner: $exp_grompp mdrun  -deffnm $tpr -v 
+## For older version of Gromacs (such as 5.1.2), you may need to use it in the following mannner: $exp_grompp mdrun  -deffnm $tpr -v 
 
 
 rm table_*.xvg 
